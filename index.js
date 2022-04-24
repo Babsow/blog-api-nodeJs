f738100f24b26b6d5ee350ce227567b154cee703
babasow <babasow120@gmail.com>
2022-04-24 22:11:08 +0200

    Basic server

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = process.env.port || 5000
+
+app.listen(port,()=> console.log('Listening on port : ${port}'))
